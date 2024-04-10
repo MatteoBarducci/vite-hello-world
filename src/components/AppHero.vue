67398c336c9eaeef73f9ed81f6012b9e3c2097ce
MatteoBarducci <mbardu1301@gmail.com>
2024-04-10 17:38:06 +0200

    Bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+        name: 'AppHero'
+    }
+</script>
+
+<template>
+    <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/bootstrap-jumbotron-19.png">
+</template>
